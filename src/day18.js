960b638e273c93a246df1e01b9fe6081937e60d3
Dan Wood <daniel4wood@icloud.com>
2020-12-18 17:44:08 +1100

    Day 18 finished

--- src/day18.js
+++ src/day18.js
@@ -2,16 +2,116 @@
 // ======
 
 function part1(input) {
-  const nums = input.split("\n").map(Number);
+  const rows = input.split("\n");
 
-  return -1;
+  const results = rows.map((row) => {
+    const rowArray = row
+      .split("")
+      .filter((c) => c !== " ")
+      .map((c) => (isNaN(Number(c)) ? c : Number(c)));
+
+    const arr = [];
+    let depth = 0;
+    let curr = 0;
+    let currOperation = "+";
+    for (let i = 0; i < rowArray.length; i++) {
+      const element = rowArray[i];
+      switch (element) {
+        case "(":
+          arr.push({ num: curr, op: currOperation, depth: depth });
+          curr = 0;
+          currOperation = "+";
+          depth++;
+          break;
+        case ")":
+          const last = arr.pop();
+          curr = eval(curr + last.op + last.num);
+          depth--;
+          break;
+        case "+":
+        case "*":
+          currOperation = element;
+          break;
+        default:
+          // Is a number
+          curr = eval(curr + currOperation + element);
+          break;
+      }
+    }
+    return curr;
+  });
+  return results.reduce((acc, curr) => acc + curr);
 }
 
 // Part 2
 // ======
 
 function part2(input) {
-  return -1;
+  const rows = input.split("\n");
+
+  const results = rows.map((row) => {
+    const elements = row
+      .split("")
+      .filter((c) => c !== " ")
+      .map((c) => (isNaN(Number(c)) ? c : Number(c)));
+
+    const arr = [];
+    let curr = 0;
+    let currOperation = "+";
+    let waiting = false;
+    elements.forEach((element) => {
+      switch (element) {
+        case "(":
+          arr.push({ num: curr, op: currOperation, waiting });
+          curr = 0;
+          currOperation = "+";
+          waiting = false;
+          break;
+        case ")":
+          let last;
+          if (waiting) {
+            last = arr.pop();
+            curr = eval(curr + last.op + last.num);
+            waiting = false;
+          }
+          last = arr.pop();
+          curr = eval(curr + last.op + last.num);
+          waiting = last.waiting;
+          break;
+        case "+":
+          currOperation = element;
+          break;
+        case "*":
+          if (waiting) {
+            const last = arr.pop();
+            curr = eval(curr + element + last.num);
+            currOperation = element;
+            waiting = false;
+          }
+          arr.push({ num: curr, op: element, waiting });
+          curr = 0;
+          currOperation = "+";
+          waiting = true;
+          break;
+        default:
+          // Is a number
+          curr = eval(curr + currOperation + element);
+          break;
+      }
+    });
+
+    // Clean up any leftover matching to do after end of input
+    let last;
+    while (arr.length) {
+      console.log("hi");
+      last = arr.pop();
+      curr = eval(curr + last.op + last.num);
+      waiting = last.waiting;
+    }
+    return curr;
+  });
+
+  return results.reduce((acc, curr) => acc + curr);
 }
 
 module.exports = { part1, part2 };
--- src/day18.test.js
+++ src/day18.test.js
@@ -1,11 +1,39 @@
 const day18 = require("./day18");
 
-const input1 = "";
+const input0 = "1 + (2 * 3) + (4 * (5 + 6))";
+const input1 = "2 * 3 + (4 * 5)";
+const input2 = "5 + (8 * 3 + 9 + 3 * 4 * 3)";
+const input3 = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))";
+const input4 = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2";
 
-test("1", () => {
-  expect(day18.part1(input1)).toBe(-1);
+test("1.0", () => {
+  expect(day18.part1(input0)).toBe(51);
+});
+test("1.1", () => {
+  expect(day18.part1(input1)).toBe(26);
+});
+test("1.2", () => {
+  expect(day18.part1(input2)).toBe(437);
+});
+test("1.3", () => {
+  expect(day18.part1(input3)).toBe(12240);
+});
+test("1.4", () => {
+  expect(day18.part1(input4)).toBe(13632);
 });
 
-test("2", () => {
-  expect(day18.part2(input1)).toBe(-1);
+test("2.0", () => {
+  expect(day18.part2(input0)).toBe(51);
+});
+test("2.1", () => {
+  expect(day18.part2(input1)).toBe(46);
+});
+test("2.2", () => {
+  expect(day18.part2(input2)).toBe(1445);
+});
+test("2.3", () => {
+  expect(day18.part2(input3)).toBe(669060);
+});
+test("2.4", () => {
+  expect(day18.part2(input4)).toBe(23340);
 });
